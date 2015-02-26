6a7b5e863d3354054d5add5d1ef5d21dc8634d28
Einstein <przemekworld@gmail.com>
2015-02-26 18:40:57 +0100

    Dodanie testu.

--- Test/TestInterfaceToXML.cs
+++ Test/TestInterfaceToXML.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Xml;
 using System.IO;
+using System.Collections;
+using System.Linq;
 
 namespace Test
 {
@@ -23,6 +25,8 @@ namespace Test
     [TestClass]
     public class TestInterfaceToXML
     {
+        const string XMLFILENAME = "test.xml";
+
         [TestMethod]
         public void Serialize()
         {
@@ -31,15 +35,37 @@ namespace Test
             lista.Add(new Person { FirstName = "Przemek" });
             lista.Add(new Person { FirstName = "Jola" });
 
-            InterfaceToXML.XMLInterfaceSerialization.Serialize<IPerson>(lista, XmlWriter.Create("test.xml"));
+            InterfaceToXML.XMLInterfaceSerialization.Serialize<IPerson>(lista, XmlWriter.Create(XMLFILENAME));
 
             const string endResult = "<?xml version=\"1.0\" encoding=\"utf-8\"?><IPersonRoot><IPerson><FirstName>Przemek</FirstName></IPerson><IPerson><FirstName>Jola</FirstName></IPerson></IPersonRoot>";
 
-            StreamReader sr = new StreamReader("test.xml");
+            StreamReader sr = new StreamReader(XMLFILENAME);
 
             var actualResult = sr.ReadLine();
 
             Assert.AreEqual(endResult, actualResult);
+        }
+
+
+        [TestMethod]
+        public void Deserialize()
+        {
+            var endResult = new List<IPerson>();
+
+            endResult.Add(new Person { FirstName = "Przemek" });
+            endResult.Add(new Person { FirstName = "Jola" });
+
+            var actualResult = InterfaceToXML.XMLInterfaceSerialization.Deserialize(typeof(IPerson), XMLFILENAME);
+
+            Assert.AreEqual(endResult.Count, endResult.Count);
+
+            foreach (var end in endResult)
+            {
+                Assert.AreEqual(true, actualResult.Cast<IPerson>().Any(l => l.FirstName == end.FirstName));
+            }
+
+
+            
         }
     }
 }
